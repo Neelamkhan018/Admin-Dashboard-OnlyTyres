6f152664584540f784db41f913663a928d4c71a3
Neelam <neelamkhan1008@gmail.com>
2025-06-05 15:15:31 +0530

    Changes done

--- src/Component/Add-product/Add-product.js
+++ src/Component/Add-product/Add-product.js
@@ -348,7 +348,16 @@ const pcdOptions = ['98mm', '100mm', '108mm', '110mm', '112mm', '114.3mm', '115m
 
 
   const SpeedRating1= ['Q : up to 160 km/h', 'R: up to 170 km/h',' S:up to 180 km/h', "T: up to 190 km/h" , " H: up to 210 km/h ", " V: up to 240 km/h"];
-  const LoadCapacity1 = [47, 52, 54, 58, 62, 65, 69, 73, 82, 85, 88, 91, 94, 95, 98, 99, 100, 104, 108] 
+  const LoadCapacity1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+41, 42, 43, 44, 45, 46, 48, 49, 50, 51,
+53, 55, 56, 57, 59, 60, 61, 63, 64, 66,
+67, 68, 70, 71, 72, 74, 75, 76, 77, 78,
+79, 80, 81, 83, 84, 86, 87, 89, 90, 92,
+93, 96, 97 , 98 , 99, 100
+] 
   const Quantity = [1,2,3,4,5,6,7,8,9,10];
   const Material = ["Steel","Nylon"];
   const warrantyOptions = ['1 year','2 years','3 years','4 years','5 years','6 years','7 years','8 years','9 years','10 years',
